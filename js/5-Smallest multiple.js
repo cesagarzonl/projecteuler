b4b562c0536c3d9d99eb8e5ef2fa2dac2cb8cd58
cesagarzonl <cigarzon8@misena.edu.co>
2017-09-27 17:16:51 -0500

    solucion del 5 232792560

---
+++ js/5-Smallest multiple.js
@@ -0,0 +1,20 @@
+var multi = 0
+var mutii = 0 
+var ss = 0
+
+for (var i = 1; i < 1000000000; i++) {
+	for (var a = 1; a <= 20; a++) {
+		mutii = i%a
+		if (mutii == 0  ) {
+			ss += 1
+			if (ss == 20 ) {
+				alert(i)
+			}
+		}
+		else {
+			ss = 0
+		}
+	}
+}
+
+//232792560
